7e9180432dddd17a66c1d387a6d525a891809bde
trung-nm-2606 <trung.nm.2606@gmail.com>
2022-02-25 20:11:46 +0700

    Add Axios interceptors to redirect login when unauthenticated

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,12 +3,22 @@ import { useDispatch } from 'react-redux';
 import AppRoutes from "./AppRoutes";
 import Layout from './Layout';
 import appRedux from './redux/app';
+import axios from 'axios';
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(appRedux.actions.setLoginAt(new Date()));
+    axios.interceptors.response.use((response) => {
+      return response;
+    }, (error) => {
+      Promise.reject(error);
+      if (error.response.status === 401) {
+        console.log('Unauthenticated User')
+        window.location.reload();
+      }
+    });
   }, [/* componentDidMount */]);
 
   return (
