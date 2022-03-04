ee1baac1a2e86a2a30acc6d382019830c2c6760b
trung-nm-2606 <trung.nm.2606@gmail.com>
2022-03-04 15:25:49 +0700

    Migrate Organization clean up

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const appApi = require('./app');
 const usersApi = require('./users');
-const organizationsApi = require('./organizations');
 const membersApi = require('./members');
 
 const api = express.Router();
 
 api.use('/users', usersApi);
-api.use('/groups', organizationsApi);
 api.use('/members', membersApi);
 api.use('/app', appApi);
 // api.use('*', (req, res) => res.status(404).json({ oper: { status: 'failed', message: 'API endpoint Not found' } }));
