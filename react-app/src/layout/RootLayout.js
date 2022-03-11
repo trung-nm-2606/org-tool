52bad06899aafa824d372c397bb9f535600a9e29
trung-nm-2606 <trung.nm.2606@gmail.com>
2022-03-11 18:29:40 +0700

    Refactor GroupSwitcher

--- react-app/src/layout/RootLayout.js
+++ react-app/src/layout/RootLayout.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { PersonCircle, ArrowRightCircleFill, Power, PlusCircle, GearWideConnected } from 'react-bootstrap-icons';
 
 const RootLayout = ({ children }) => {
   const authUser = useSelector(state => state.app.authUser);
   const [inGroup, setInGroup] = useState(false);
   const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const { pathname = '' } = location;
@@ -23,17 +24,32 @@ const RootLayout = ({ children }) => {
       <div className="top-nav p-2 w-100 bg-light d-flex align-items-center flex-row">
         <div className="top-nav w-100 d-flex align-items-center flex-row">
           <PersonCircle role="button" className="ms-2 me-2 user-icon" size={24} />
-          <div role="button" className="workspace-switcher bg-success bg-opacity-10 pt-1 pb-1 ps-2 ps-sm-4 pe-2 pe-sm-3 ms-2 me-2 d-flex flex-row align-items-center justify-content-center">
+          <div
+            role="button"
+            className="workspace-switcher bg-success bg-opacity-10 pt-1 pb-1 ps-2 ps-sm-4 pe-2 pe-sm-3 ms-2 me-2 d-flex flex-row align-items-center justify-content-center"
+            onClick={() => {
+              const pathname = inGroup ? '/group-management' : '/group/dashboard';
+              navigate(pathname);
+            }}
+          >
             <div className="active-group d-flex flex-column align-items-start me-3">
               <small className="text-muted second-label d-none d-sm-block">Active Group</small>
-              <NavLink
+              <div
                 className="none-decoration d-flex flex-row align-items-center"
                 to={inGroup ? '/group-management' : '/group/dashboard'}
               >
                 <span className="label text-success">
                   {authUser?.activeGroup?.name}
                 </span>
-              </NavLink>
+              </div>
+              {/* <NavLink
+                className="none-decoration d-flex flex-row align-items-center"
+                to={inGroup ? '/group-management' : '/group/dashboard'}
+              >
+                <span className="label text-success">
+                  {authUser?.activeGroup?.name}
+                </span>
+              </NavLink> */}
             </div>
             <ArrowRightCircleFill className="text-success" size={24} />
           </div>
