e71335950c26609c470577fb50bf56af03a6cbb5
trung-nm-2606 <trung.nm.2606@gmail.com>
2022-03-12 21:58:42 +0700

    Add FundDao

--- db/funds.sql
+++ db/funds.sql
@@ -3,14 +3,16 @@ create table funds
 (
     pk              int(11) unsigned auto_increment
         primary key,
-    name            varchar(150)                                 not null,
-    description     varchar(500)                                 null,
-    organization_pk int(11) unsigned                             not null,
-    balance         decimal(19, 2)              default 0.00     not null,
-    currency        enum ('vnd', 'usd')         default 'vnd'    not null,
-    status          enum ('active', 'archived') default 'active' not null,
-    created_by      int(11) unsigned                             not null,
-    updated_by      int(11) unsigned                             not null,
+    name            varchar(150)                                            not null,
+    description     varchar(500)                                            null,
+    organization_pk int(11) unsigned                                        not null,
+    balance         decimal(19, 2)              default 0.00                not null,
+    currency        enum ('vnd', 'usd')         default 'vnd'               not null,
+    status          enum ('active', 'archived') default 'active'            not null,
+    created_by      int(11) unsigned                                        not null,
+    created_at      datetime                    default current_timestamp() not null,
+    updated_by      int(11) unsigned                                        not null,
+    updated_at      datetime                    default current_timestamp() not null,
     constraint fund_organizations_pk_fk
         foreign key (organization_pk) references organizations (pk)
             on delete cascade,
--- src/module/fund/dao.js
+++ src/module/fund/dao.js
@@ -1,3 +1,40 @@
+const db = require('../../shared/db');
+
 const Dao = {};
+
+Dao.findFundsByOrganizationPk = async (origanizationPk) => {
+  try {
+    const query = `select * from funds where organization_pk = ?`;
+    const [funds] = await db.execute(query, [origanizationPk]);
+    return funds;
+  } catch (e) {
+    throw new DaoError(`Cannot find funds for group(${origanizationPk})`, e);
+  }
+};
+
+Dao.createFund = async (origanizationPk, fund, userPk) => {
+  const { name, description, balance, currency  } = fund;
+
+  try {
+    const query = `insert into fund(name, description, organization_pk, balance, currency, created_by, updated_by)
+    values(?,?,?,?,?,?,?)`;
+    const [result] = await db.execute(query, [
+      name, description, origanizationPk, balance, currency, userPk, userPk
+    ])
+    return result.insertId > 0;
+  } catch (e) {
+    throw new DaoError(`Cannot create fund for group(${origanizationPk})`, e);
+  }
+};
+
+Dao.deleteFund = async (fundPk) => {
+  try {
+    const query = `delete from funds where pk = ?`;
+    await db.execute(query, [fundPk]);
+    return true;
+  } catch (e) {
+    throw new DaoError(`Cannot find funds for group(${origanizationPk})`, e);
+  }
+};
 
 module.exports = Dao;
