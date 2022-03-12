2980b6e839a08906e6a8046784474ff8f3b6078e
trung-nm-2606 <trung.nm.2606@gmail.com>
2022-03-12 22:08:51 +0700

    Add FundEventDao

--- src/module/fund/dao.js
+++ src/module/fund/dao.js
@@ -16,7 +16,7 @@ Dao.createFund = async (origanizationPk, fund, userPk) => {
   const { name, description, balance, currency  } = fund;
 
   try {
-    const query = `insert into fund(name, description, organization_pk, balance, currency, created_by, updated_by)
+    const query = `insert into funds(name, description, organization_pk, balance, currency, created_by, updated_by)
     values(?,?,?,?,?,?,?)`;
     const [result] = await db.execute(query, [
       name, description, origanizationPk, balance, currency, userPk, userPk
@@ -34,6 +34,41 @@ Dao.deleteFund = async (fundPk) => {
     return true;
   } catch (e) {
     throw new DaoError(`Cannot find funds for group(${origanizationPk})`, e);
+  }
+};
+
+Dao.findFundEventsByFundPk = async (fundPk) => {
+  try {
+    const query = `select * from fund_events where fund_pk = ?`;
+    const [fundEvents] = await db.execute(query, [fundPk]);
+    return fundEvents;
+  } catch (e) {
+    throw new DaoError(`Cannot find funds for group(${origanizationPk})`, e);
+  }
+};
+
+Dao.createFundEvent = async (fundPk, fundEvent, userPk) => {
+  const { name, description, amountPerMember } = fundEvent;
+
+  try {
+    const query = `insert into fund_events(name, description, fund_pk, amount_per_member, created_by, updated_by)
+    values(?,?,?,?,?,?)`;
+    const [result] = await db.execute(query, [
+      name, description, fundPk, amountPerMember, userPk, userPk
+    ])
+    return result.insertId > 0;
+  } catch (e) {
+    throw new DaoError(`Cannot create fund event fund(${fundPk})`, e);
+  }
+};
+
+Dao.archiveFundEventByPk = async (fundEventPk, reason) => {
+  try {
+    const query = `update fund_events set status = 'archived', status_reason = ? where pk = ?`;
+    await db.execute(query, [fundEventPk, reason]);
+    return true;
+  } catch (e) {
+    throw new DaoError(`Cannot archive fund event(${fundEventPk})`, e);
   }
 };
 
