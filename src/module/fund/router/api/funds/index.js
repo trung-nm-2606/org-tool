d25a6b5e75c7f6adc54f5fdfc1f04e6f1e03b22d
trung-nm-2606 <trung.nm.2606@gmail.com>
2022-03-13 10:46:43 +0700

    Add FundsAPI

--- src/module/fund/router/api/funds/index.js
+++ src/module/fund/router/api/funds/index.js
@@ -8,4 +8,25 @@ const Validator = require('../../../validator');
 
 const fundsRouter = express.Router();
 
+fundsRouter.post('/:organizationPk', [
+  ApiView.api,
+  session.authenticateUser,
+  Controller.createFund,
+  BaseController.render
+]);
+
+fundsRouter.get('/:organizationPk/get-all', [
+  ApiView.api,
+  session.authenticateUser,
+  Controller.getFunds,
+  BaseController.render
+]);
+
+fundsRouter.delete('/:organizationPk/:fundPk/delete', [
+  ApiView.api,
+  session.authenticateUser,
+  Controller.deleteFund,
+  BaseController.render
+]);
+
 module.exports = fundsRouter;
